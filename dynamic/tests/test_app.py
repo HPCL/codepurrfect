8a8fa1cbbba8ac85197b0e865e48ddb0add96694
Shahid Hussain <shussain@apollo.cs.uoregon.edu>
2020-12-22 14:49:50 -0800

    Test script revised

--- dynamic/tests/test_app.py
+++ dynamic/tests/test_app.py
@@ -1,3 +1,6 @@
-from app import index
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from src.app import index
 def test_index():
     assert index() == "Hellow, Code Analysis"
