5f9553deaaf18aa16448637cd41839da1450ee9a
Shahid Hussain <shussain@apollo.cs.uoregon.edu>
2020-12-22 12:40:23 -0800

    Change in source file

--- dynamic/src/app.py
+++ dynamic/src/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
+app.route('/')
 def index():
     return "Hellow, Code Analysis"
 
-if __name__=="__main__":
+#if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
