ddf4b8c9bbbe0b64c886f4f87955a7b2cbca597b
Bosco Ndemeye <ndemeye@apollo.cs.uoregon.edu>
2021-06-08 15:00:36 -0700

    rename AliasCallPointers to CallGraphWithIndirectCalls

---
+++ build/CallGraphWithIndirectCalls/CMakeFiles/CallGraphWithIndirectCalls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CallGraphWithIndirectCalls.dir/CallGraphWithIndirectCalls.cpp.o"
+  "libCallGraphWithIndirectCalls.pdb"
+  "libCallGraphWithIndirectCalls.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CallGraphWithIndirectCalls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CallGraphWithIndirectCalls/cmake_install.cmake
+++ build/CallGraphWithIndirectCalls/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/users/ndemeye/xSDK/code-analysis/static/pointer-call-analysis
+# Install script for directory: /home/users/ndemeye/xSDK/code-analysis/static/pointer-call-analysis/CallGraphWithIndirectCalls
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -42,18 +42,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/users/ndemeye/xSDK/code-analysis/static/pointer-call-analysis/build/CallGraphWithIndirectCalls/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/users/ndemeye/xSDK/code-analysis/static/pointer-call-analysis/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
