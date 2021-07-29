b362eb1944a652d418e3a37518a692a99b4b014f
ndemeyembosco <ndemeye@cs.uoregon.edu>
2021-07-28 17:28:51 -0700

    implement mccabe base-measures. problems with mu2

---
+++ src/static/xsdk-metrics/promise-mccabe-c/test.cpp
@@ -0,0 +1,19 @@
+
+int max(int x, int y)
+{
+	return x > y ? x : y; 
+	
+}
+
+int func1(int w, int x, int y)
+{
+	return max(w+x, x+y);
+}
+
+int main()
+{
+    int w = 2; 
+	int x = 1; 
+	int y = 0;
+	func1(w, x, y);
+}	
