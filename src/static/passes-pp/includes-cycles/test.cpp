2cc06ceed687b9470f21f6d0011312f86340a09e
Bosco Ndemeye <ndemeye@cs.uoregon.edu>
2022-03-15 14:53:18 -0700

    add pass to check circular dependencies; add instructions for adding new pass

---
+++ src/static/passes-pp/includes-cycles/test.cpp
@@ -0,0 +1,87 @@
+#include "test.h"
+#include "utils.hpp"
+// using namespace std;
+ 
+int main () {
+   // local variable declaration:
+   char grade = 'D';
+   int e = 0;
+
+   switch(grade) {
+      case 'A' :
+         e = 1 + 2; 
+         break;
+      case 'B' :
+      case 'C' :
+         e = 1 + 2;
+      case 'D' :
+         break;
+      case 'F' :
+         e = 1 + 2;
+         break;
+      default :
+         e = 1 + 4;
+   }
+
+   char grade_2 = 'D'; 
+
+   switch(grade) {
+      case 'A' :
+         e = 1 + 2; 
+         break;
+      case 'B' :
+      case 'C' :
+         e = 1 + 2;
+         break;
+      case 'D' :
+         e = 1 + 2;
+      case 'F' :
+         e = 1 + 2;
+         break;
+      // default :
+      //    e = 1 + 4;
+   }
+
+   char grade_3 = 'D'; 
+   char grade_4 = 'D';
+
+   switch(grade_3) {
+      case 'A' :
+         e = 1 + 2; 
+         break;
+      case 'B' :
+      case 'C' :
+         e = 1 + 2;
+            switch(grade_4) {
+               case 'A' :
+                  e = 1 + 2; 
+                  break;
+               case 'B' :
+               case 'C' :
+                  e = 1 + 2;
+                  break;
+               case 'D' :
+                  e = 1 + 2;
+               case 'F' :
+                  e = 1 + 2;
+                  break;
+               // default :
+               //    e = 1 + 4;
+            }
+         break;
+      case 'D' :
+         e = 1 + 2;
+         break;
+      case 'F' :
+         e = 1 + 2;
+         break;
+      // default :
+      //    e = 1 + 4;
+   }
+
+
+
+
+ 
+   return 0;
+}
